885913784ed3c4112635375d81584f7f77789204
Joey <54172386+JavaJava14@users.noreply.github.com>
2020-05-17 02:25:33 -0500

    Add helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,17 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
+  get '/' do
     erb :welcome
   end
 
+  helpers do
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+  end
 end
